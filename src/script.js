c75ba748ff76183a5e96649f58124b9f83cb19d9
Massimedj <135154664+Massimedj@users.noreply.github.com>
2025-06-20 15:33:30 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -430,7 +430,7 @@
             <div class="max-w-5xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
                 <h1 class="text-4xl font-extrabold text-center text-blue-700 mb-8 leading-tight">
                     Marre des casse-têtes<img src="Images/explosion.png" alt="emoji casse-tête" class="inline-block w-12 h-12 align-middle mx-1">pour organiser vos tournois ?<br>
-                    Cette App est là pour simplifier la vie des organisateurs de tournois ! <img src="Images/content.png" alt="emoji casse-tête" class="inline-block w-15 h-12 align-middle mx-1">
+                    Cette App est là pour simplifier la vie des organisateurs de tournois !<img src="Images/content.png" alt="emoji casse-tête" class="inline-block w-15 h-12 align-middle mx-1">
                 </h1>
 
                 <p class="text-xl text-gray-700 text-center mb-12">
